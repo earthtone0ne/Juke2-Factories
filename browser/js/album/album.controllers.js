6fac3539a1ebfc994fd99e209f6bce7d52572268
Stephanie Adams <adamsstephaniem@gmail.com>
2016-07-07 16:22:21 -0400

    Created AlbumFactory

--- browser/js/album/album.controllers.js
+++ browser/js/album/album.controllers.js
@@ -1,28 +1,17 @@
 'use strict';
 
-juke.controller('AlbumCtrl', function ($scope, $http, $rootScope, $log, StatsFactory) {
-
-  // load our initial data
-  $http.get('/api/albums/')
-  .then(function (res) { return res.data; })
-  .then(function (albums) {
-    return $http.get('/api/albums/' + albums[0].id); // temp: get one
-  })
-  .then(function (res) { return res.data; })
-  .then(function (album) {
-    album.imageUrl = '/api/albums/' + album.id + '/image';
-    album.songs.forEach(function (song, i) {
-      song.audioUrl = '/api/songs/' + song.id + '/audio';
-      song.albumIndex = i;
-    });
-    $scope.album = album;
-    return StatsFactory.totalTime(album);
-  })
+juke.controller('AlbumCtrl', function ($scope, $http, $rootScope, $log, StatsFactory, AlbumFactory) {
+    AlbumFactory.fetchAll()
+    .then(AlbumFactory.fetchById(albums))
+    .then(function (album) {
+      $scope.album = album;
+      return StatsFactory.totalTime(album);
+    })
   .then(function (albumDuration) {
     $scope.fullDuration = albumDuration;
   })
   .catch($log.error); // $log service can be turned on and off; also, pre-bound
-
+}
   // main toggle
   $scope.toggle = function (song) {
     if ($scope.playing && song === $scope.currentSong) {
